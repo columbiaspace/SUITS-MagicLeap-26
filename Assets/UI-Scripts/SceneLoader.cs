1c567caae6b8f13fc4425f322432d339dbbef598
hibaa8 <hibaaltaf98@gmail.com>
2026-04-21 22:25:44 -0400

    move Egress, Ingress scenes to final_scenes, add Egress, Ingress button to mission scene, remove Egress_Ingress scene

--- Assets/UI-Scripts/SceneLoader.cs
+++ Assets/UI-Scripts/SceneLoader.cs
@@ -1,10 +1,24 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadScene(string sceneName)
+    public void LoadScene(string sceneNameOrPath)
     {
-        SceneManager.LoadScene(sceneName);
+        if (!string.IsNullOrEmpty(sceneNameOrPath) && sceneNameOrPath.StartsWith("Assets/", StringComparison.Ordinal))
+        {
+            var path = sceneNameOrPath.EndsWith(".unity", StringComparison.OrdinalIgnoreCase)
+                ? sceneNameOrPath
+                : sceneNameOrPath + ".unity";
+            int buildIndex = SceneUtility.GetBuildIndexByScenePath(path);
+            if (buildIndex >= 0)
+            {
+                SceneManager.LoadScene(buildIndex);
+                return;
+            }
+        }
+
+        SceneManager.LoadScene(sceneNameOrPath);
     }
 }
